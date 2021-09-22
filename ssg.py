d1f541ac38dc370d65c62c4ea65390f20078ed40
Skurzynowski <sebastian.kurzynowski@gmail.com>
2021-09-22 11:35:15 +0200

    Finish first module

--- ssg.py
+++ ssg.py
@@ -4,10 +4,9 @@ from ssg.site import Site
 
 def main(source="content",dest="dist"):
     config = {"source":source,"dest":dest}
-    s = Site( **config )
-    s.build()
+    Site(**config).build()
 
-typer.run( main )
+typer.run(main)
 
 
 

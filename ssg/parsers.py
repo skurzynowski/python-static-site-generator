654befc4ed00267c3d585f7d8ded230352070e94
Skurzynowski <sebastian.kurzynowski@gmail.com>
2021-09-22 12:24:51 +0200

    Add copy

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,5 +1,6 @@
 from typing import List
 from pathlib import Path
+import shutil
 
 
 class Parser:
@@ -19,4 +20,7 @@ class Parser:
         full_path = dest / path.with_suffix(ext).name 
         with open(full_path, "w") as file:
             return file.write(content)
+
+    def copy(self, path, source, dest):
+        shutil.copy2(path, dest / path.relative_to(source))
 

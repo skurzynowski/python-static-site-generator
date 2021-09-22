689533073d77b9089f88eb8d767533a5bc6c4d9a
Skurzynowski <sebastian.kurzynowski@gmail.com>
2021-09-22 12:01:43 +0200

    Create read function

---
+++ ssg/parsers.py
@@ -0,0 +1,20 @@
+from typing import List
+from pathlib import Path
+
+
+class Parser:
+    extensions: List[str] = []
+
+    def valid_extension(self, extension):
+        return extension in self.extensions
+
+    def parse(self, path:Path, source:Path, dest:Path):
+        raise NotImplementedError;
+
+    def read(self,path):
+        with open(path, "r") as file:
+            return file.read()
+
+    def write(self, path, dest, content, ext=".html"):
+        full_path = dest / path.with_suffix(ext).name 
+

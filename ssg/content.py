22c43aa82cf5df143a0e728f8d45027bf8203fca
Skurzynowski <sebastian.kurzynowski@gmail.com>
2021-09-23 10:38:28 +0200

    Add basic properties

--- ssg/content.py
+++ ssg/content.py
@@ -1,10 +1,10 @@
 import re
-from yaml import (load, FullLoader)
+from yaml import load, FullLoader
 from collections.abc import Mapping
 
 class Content(Mapping):
-    __delimeter = "^(?:-|\+){3}\s*$"
-    __regex = re.compile(__delimeter, re.MULTILINE)
+    __delimiter = r"^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
     pass
 
 

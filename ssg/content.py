d4011fc487e4cc439ee4b1404213a76ae5166000
Skurzynowski <sebastian.kurzynowski@gmail.com>
2021-09-23 10:46:43 +0200

    Add load method

--- ssg/content.py
+++ ssg/content.py
@@ -5,6 +5,10 @@ from collections.abc import Mapping
 class Content(Mapping):
     __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
+    def load(self, cls, string):
+        _, fm, content = self.__regex.split(string, 2)
+        load(fm, Loader=FullLoader)
+        return cls(metadata, content)
     pass
 
 

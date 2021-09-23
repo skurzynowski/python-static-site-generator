f4ed6052ba1466d13ca3568488b1aba73edec9c2
Skurzynowski <sebastian.kurzynowski@gmail.com>
2021-09-23 10:33:37 +0200

    Create content class

---
+++ ssg/content.py
@@ -0,0 +1,13 @@
+import re
+from yaml import (load, FullLoader)
+from collections.abc import Mapping
+
+class Content(Mapping):
+    __delimeter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimeter, re.MULTILINE)
+    pass
+
+
+
+
+

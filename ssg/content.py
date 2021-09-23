1831ed841175895266794eccce319a0bf54a017d
Skurzynowski <sebastian.kurzynowski@gmail.com>
2021-09-23 11:04:34 +0200

    Add decorator

--- ssg/content.py
+++ ssg/content.py
@@ -5,9 +5,11 @@ from collections.abc import Mapping
 class Content(Mapping):
     __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
+
+    @classmethod
     def load(self, cls, string):
-        _, fm, content = self.__regex.split(string, 2)
-        load(fm, Loader=FullLoader)
+        _, fm, content = cls.__regex.split(string, 2)
+        metadata = load(fm, Loader=FullLoader)
         return cls(metadata, content)
     pass
 
